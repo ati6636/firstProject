21fb69e0233831017bae2be40bcd8a2a9f08d525
sedatişlek <sdt6@hotmail.com>
2022-06-23 01:05:52 +0300

    Controller - Route - view işlemleriyle index, about,  contact sayfalarını oluşturuldu.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('front.index');
 });
+
+Route::get('/about',[HomeController::class,'index']);
+
+Route::get('/contact','HomeController@contact');
